ff12bfcecaf09c46d8375cd1b77992066f66fd0f
codepundit1 <jahid35-266@diu.edu.bd>
2021-11-29 01:51:43 +0600

    Correct many things and Done user Athentication such as login, logout, registration

--- management/urls.py
+++ management/urls.py
@@ -3,5 +3,11 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path("register/", views.register, name="register"),
+    path("register/", views.user_registration, name = "register"),
+    path("login/", views.user_login, name="login"),
+    path("contact/", views.contact, name="contact"),
+    # after user login
+    path("home/", views.user_home, name="home"),
+    path("logout/", views.user_logout, name="logout"),
+    
 ]
--- management/views.py
+++ management/views.py
@@ -1,10 +1,72 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import django
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, request
+#auth
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
+# Registration
+def user_registration(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {
+        'form' : form,
+    }
+
+    return render(request, "register.html", context=context)
+
+
+# Login
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.info(request, "Successfully logged in")
+            return redirect('/')
+        else:
+            messages.info(request, 'Username or password is incorrect!')
+
+    context = {
+        "form" : ""
+    }
+    return render(request, "login.html", context=context)
+
+
+# logout
+def user_logout(request):
+    logout(request)
+    return redirect("login")
 
 def index(request):
     return render(request, "index.html")
 
-def register(request):
-    return render(request, "register.html")
+
+
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+
+
+
+# after user login
+
+def user_home(request):
+    return render(request, "user/home.html")
